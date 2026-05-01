9ef9c97144237cdcc608f2ccbd3a34eb2bdf6ce9
Issa Oulkhir <ioulkhir@e2r7p5.1337.ma>
2026-05-01 17:55:51 +0100

    now we are using micro-services

---
+++ services/main_app/app/api/createAdmin/route.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt"
+
+export async function GET(req: NextRequest) {
+    try {
+        const admin: User = await prisma.user.findFirst({
+            where: {
+                role: "ADMIN"
+            }
+        });
+
+        if (admin) {
+            return NextResponse.json({
+                success: false,
+                error: "admin already exists !"
+            })
+        }
+
+        await prisma.user.create({
+            data: {
+                firstname: process.env.ADMIN_FIRSTNAME,
+                lastname: process.env.ADMIN_LASTNAME,
+                phoneNumber: process.env.ADMIN_PHONE,
+                email: process.env.ADMIN_EMAIL,
+                password: await bcrypt.hash(
+                    process.env.ADMIN_PASS || "very-secure-admin-password", 10
+                ),
+                role: "ADMIN"
+            }
+        })
+
+        return NextResponse.json({
+            success: true,
+            error: "admin created successfully !"
+        })
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({
+            success: false,
+            error: "Something went wrong"
+        })
+    }
+}
+

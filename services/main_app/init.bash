6f7323c0b796201cf4dfd2776a11eae4c91168fc
Zoom <yassine.zirri2@gmail.com>
2026-06-22 22:55:27 +0100

    docker compose works, with proxy server, and made some small fixes

--- services/main_app/init.bash
+++ services/main_app/init.bash
@@ -1,14 +1,6 @@
-#!/bin/bash
-
-echo "waiting for mariadb ..."
-
-# until ss -tln | grep -q ':3306'; do sleep 1; done;
-sleep 10;
-
-echo "lets gooo, mariadb is alive and listening"
+#!/bin/sh
+set -e
 
 npx prisma db push
 npx prisma generate
-npm run build
-
 exec npm start

4e61056ec3889a9276cf0f5f2ed71ed39d616754
Issa Oulkhir <ioulkhir@e3r1p17.1337.ma>
2026-08-28 11:09:08 +0100

    deleted dev-level files we don't need anymore

--- services/mariadb/run_container.bash
+++
@@ -1,4 +0,0 @@
-
-docker build .
-docker run -d --restart always -p 3306:3306 <IMAGE_ID>
-
--- services/reverse_proxy/run_container.bash
+++
@@ -1,6 +0,0 @@
-
-docker build .
-docker run -d --restart always -p 3002:80 <IMAGE_ID>
-
-# thus we listen in localhost:3002 in dev mode
-
